56cf6533418ec9ccde23422f99e148b1863e3851
Pablo Henrique <pablohdsouza.dev@gmail.com>
2024-12-26 18:11:06 -0300

    feat: add configs and components related to auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,8 @@
 import createMiddleware from "next-intl/middleware";
 import { routing } from "./i18n/routing";
 
+export { auth as authMiddleware } from "@/auth";
+
 export default createMiddleware(routing);
 
 export const config = {
