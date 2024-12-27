49b727f13c12a74ad28945baa6f192c7320fd97a
Pablo Henrique <pablohdsouza.dev@gmail.com>
2024-12-27 15:33:14 -0300

    chore: add prisma, auth prisma adapter and config prisma client

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth";
 import Discord from "next-auth/providers/discord";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/prisma";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [Discord],
 });
