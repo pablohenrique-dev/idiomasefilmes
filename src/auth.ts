1d850e5ca99b125c53e25e2c26b6680ca14c855e
Pablo Henrique <pablohdsouza.dev@gmail.com>
2025-01-08 22:47:08 -0300

    chore: add user id to auth

--- src/auth.ts
+++ src/auth.ts
@@ -1,9 +1,30 @@
-import NextAuth from "next-auth";
+import NextAuth, { type DefaultSession } from "next-auth";
 import Discord from "next-auth/providers/discord";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/prisma";
 
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+      // role: "ADMIN" | "USER";
+      /**
+       * By default, TypeScript merges new interface properties and overwrites existing ones.
+       * In this case, the default session user properties will be overwritten,
+       * with the new ones defined above. To keep the default session user properties,
+       * you need to add them back into the newly declared interface.
+       */
+    } & DefaultSession["user"];
+  }
+}
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [Discord],
+  callbacks: {
+    session({ session, user }) {
+      session.user.id = user.id;
+      return session;
+    },
+  },
 });
