cc6e6d62ca90622bc912bdd57cd159007644b347
Pablo Henrique <pablohdsouza.dev@gmail.com>
2025-01-17 09:45:37 -0300

    feat: add action to delete scene

---
+++ src/actions/delete-scene.ts
@@ -0,0 +1,32 @@
+"use server";
+
+import { prisma } from "@/prisma";
+
+const messages = {
+  success: {
+    pt: "Cena deletada com sucesso",
+    en: "Scene deleted successfully",
+    es: "Escena eliminada con éxito",
+  },
+  error: {
+    unknown: {
+      pt: "Ocorreu um erro ao deletar a cena. Tente novamente!",
+      en: "An error occurred while deleting the scene. Please try again!",
+      es: "Se produjo un error al eliminar la escena. ¡Intentar otra vez!",
+    },
+  },
+};
+
+export async function deleteSceneAction(slug: string, locale: string) {
+  try {
+    await prisma.scene.delete({
+      where: {
+        slug,
+      },
+    });
+
+    return [messages.success[locale as "pt" | "en" | "es"], null];
+  } catch {
+    return [null, messages.error.unknown[locale as "pt" | "en" | "es"]];
+  }
+}
