c0c67078b29c13c074ebd0526e5d90115fb663b8
Pablo Henrique <pablohdsouza.dev@gmail.com>
2025-01-17 09:52:28 -0300

    refactor: add params to getScenes function

--- src/data-access/get-scenes.ts
+++ src/data-access/get-scenes.ts
@@ -6,17 +6,33 @@ interface Data {
   amount?: number;
 }
 
+interface getScenesDataParams {
+  slug?: string;
+  take?: number;
+}
+
 interface GetScenesDataResponse {
   data: Data | null;
   error: string | null;
 }
 
-export async function getScenesData(): Promise<GetScenesDataResponse> {
+export async function getScenesData({
+  slug,
+  take,
+}: getScenesDataParams): Promise<GetScenesDataResponse> {
   try {
-    const scenes = await prisma.scene.findMany();
+    const scenes = await prisma.scene.findMany({
+      where: {
+        slug: {
+          not: slug,
+        },
+      },
+      take,
+    });
 
-    return { data: { scenes }, error: null };
-  } catch {
-    return { data: null, error: "Ocorreu um erro" };
+    return { data: { scenes, amount: scenes.length }, error: null };
+  } catch (error) {
+    console.error(error);
+    return { data: null, error: "Ocorreu um erro ao buscar as cenas." };
   }
 }
