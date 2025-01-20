c852cb29032dcfff150e97b8ea7c3cc8e723815e
Pablo Henrique <pablohdsouza.dev@gmail.com>
2025-01-20 09:47:55 -0300

    chore: add new param to getSceneData function

--- src/data-access/get-scene.ts
+++ src/data-access/get-scene.ts
@@ -1,23 +1,37 @@
 import { prisma } from "@/prisma";
 import { Scene } from "@prisma/client";
 
+interface SceneWithLikeInfo extends Scene {
+  isLiked: boolean;
+}
 interface GetScenesDataResponse {
-  data: Scene | null;
+  data: SceneWithLikeInfo | null;
   error: string | null;
 }
 
 export async function getSceneData(
   slug: string,
+  userId?: string,
 ): Promise<GetScenesDataResponse> {
   try {
     const scene = await prisma.scene.findUnique({
       where: {
         slug,
       },
+      include: {
+        LikedScene: userId
+          ? {
+              where: { userId },
+              select: { id: true },
+            }
+          : false,
+      },
     });
 
     if (scene) {
-      return { data: scene, error: null };
+      const isLiked = userId ? scene.LikedScene.length > 0 : false;
+
+      return { data: { ...scene, isLiked }, error: null };
     } else {
       return { data: null, error: "Cena não encontrada" };
     }
