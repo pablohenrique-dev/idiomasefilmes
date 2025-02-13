30b8f9810bd339043a4144c53502947deacd80c5
Pablo Henrique <pablohdsouza.dev@gmail.com>
2025-02-13 20:02:54 -0300

    fix: display X icon only for wrong answers

--- src/app/[locale]/[slug]/question.tsx
+++ src/app/[locale]/[slug]/question.tsx
@@ -22,30 +22,40 @@ export function Question({ question, isAnswerVisible }: QuestionProps) {
     <div className="mt-8">
       <h3 className="mb-3 font-semibold">{question.statement}</h3>
       <div className="space-y-2">
-        {question.options?.map((option, index) => (
-          <label key={index} className="relative flex items-center space-x-2">
-            <input
-              type="radio"
-              name={`question-option-${question.id}`}
-              value={option}
-              checked={selectedOption === option}
-              onChange={() => handleOptionChange(option)}
-              disabled={isAnswerVisible}
-              className="radio-input select"
-            />
-            <span>{option}</span>
-            {question.correctAnswer === index && isAnswerVisible && (
-              <span className="rounded bg-green-200 p-[2px]">
-                <Check className="size-4 text-green-800" />
-              </span>
-            )}
-            {option !== selectedOption && isAnswerVisible && (
-              <span className="rounded bg-red-200 p-[2px]">
-                <X className="size-4 text-red-800" />
-              </span>
-            )}
-          </label>
-        ))}
+        {question.options?.map((option, index) => {
+          const isCorrect = question.correctAnswer === index;
+          const isSelected = selectedOption === option;
+          const isWrongSelection = isSelected && !isCorrect;
+
+          return (
+            <label key={index} className="relative flex items-center space-x-2">
+              <input
+                type="radio"
+                name={`question-option-${question.id}`}
+                value={option}
+                checked={isSelected}
+                onChange={() => handleOptionChange(option)}
+                disabled={isAnswerVisible}
+                className="radio-input select"
+              />
+              <span>{option}</span>
+
+              {/* Exibe o Check apenas na alternativa correta */}
+              {isCorrect && isAnswerVisible && (
+                <span className="rounded bg-green-200 p-[2px]">
+                  <Check className="size-4 text-green-800" />
+                </span>
+              )}
+
+              {/* Exibe o X apenas se o usuário selecionou uma opção errada */}
+              {isWrongSelection && isAnswerVisible && (
+                <span className="rounded bg-red-200 p-[2px]">
+                  <X className="size-4 text-red-800" />
+                </span>
+              )}
+            </label>
+          );
+        })}
       </div>
     </div>
   );
