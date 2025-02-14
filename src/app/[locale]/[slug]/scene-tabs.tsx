cbfc7e74a031d7242db6872ea70de4078d58aaba
Pablo Henrique <pablohdsouza.dev@gmail.com>
2025-02-14 09:30:14 -0300

    feat: add function to set and get active tab search param

--- src/app/[locale]/[slug]/scene-tabs.tsx
+++ src/app/[locale]/[slug]/scene-tabs.tsx
@@ -1,18 +1,17 @@
 "use client";
 
-import React from "react";
-import { Script } from "./script";
+import { Question as IQuestion } from "@/@types/question";
 import {
-  Tabs as TabsWrapper,
   TabsContent,
   TabsList,
   TabsTrigger,
+  Tabs as TabsWrapper,
 } from "@/components/ui/tabs";
-import { Question as IQuestion } from "@/@types/question";
-import { Questions } from "./questions";
-import { User } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { useTranslations } from "next-intl";
+import { useRouter, useSearchParams } from "next/navigation";
+import { Questions } from "./questions";
+import { Script } from "./script";
 
 const tabItens = [
   {
@@ -29,36 +28,52 @@ const tabItens = [
 interface SceneTabsProps {
   script: string;
   questions: IQuestion[];
-  user?: {
-    id: string;
-    role: "ADMIN" | "USER";
-  } & User;
+  sceneSlug: string;
 }
 
-export function SceneTabs({ script, questions }: SceneTabsProps) {
+export function SceneTabs({ script, questions, sceneSlug }: SceneTabsProps) {
   const t = useTranslations("SingleScenePage");
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  const tabDefaultValue = searchParams.get("active-tab") || tabItens[0].value;
+
+  const handleTabChange = (value: string) => {
+    const params = new URLSearchParams(searchParams);
+    params.set("active-tab", value);
+    router.push(`?${params.toString()}`, { scroll: false });
+  };
 
   return (
-    <TabsWrapper defaultValue={tabItens[0].value} className="mt-8 w-full">
+    <TabsWrapper defaultValue={tabDefaultValue} className="mt-8 w-full">
       <TabsList className="grid w-full grid-cols-2 gap-2 border bg-transparent md:w-fit">
-        {tabItens.map(({ value, label }) => (
-          <TabsTrigger
-            className="bg-[#F5F5F5] text-black data-[state=active]:bg-black data-[state=active]:text-white"
-            key={value}
-            value={value}
-          >
-            {label(t)}
-          </TabsTrigger>
-        ))}
+        {tabItens.map(({ value, label }) => {
+          if (value === "questions" && questions.length === 0) return null;
+          return (
+            <TabsTrigger
+              className="bg-[#F5F5F5] text-black data-[state=active]:bg-black data-[state=active]:text-white"
+              key={value}
+              value={value}
+              onClick={() => handleTabChange(value)}
+            >
+              {label(t)}
+            </TabsTrigger>
+          );
+        })}
       </TabsList>
       <TabsContent value="script" className="animate-fade-in">
         <Script script={script} />
       </TabsContent>
-      <TabsContent value="questions" className="flex animate-fade-in flex-col">
-        <SessionProvider>
-          <Questions questions={questions} />
-        </SessionProvider>
-      </TabsContent>
+      {!!questions.length && (
+        <TabsContent
+          value="questions"
+          className="flex animate-fade-in flex-col"
+        >
+          <SessionProvider>
+            <Questions questions={questions} sceneSlug={sceneSlug} />
+          </SessionProvider>
+        </TabsContent>
+      )}
     </TabsWrapper>
   );
 }
